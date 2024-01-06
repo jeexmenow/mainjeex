c8e766ffd4b26201ddc0cca21ab2a3d3c10f502d
jeexy <147101413+jeexmenow@users.noreply.github.com>
2024-01-07 02:16:32 +0300

    Delete NewsPortalSF/news/urls.py

--- NewsPortalSF/news/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . views import Posts, PostDetail, PostCreate
-
-
-urlpatterns = [
-    path('', Posts.as_view(), name='post_list'),
-    path('<int:pk>', PostDetail.as_view(), name='post_detail'),
-    path('create', PostCreate.as_view(), name='newpost'),
-    ]
